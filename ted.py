840e1810877c8c3f08fc1d1ddd42cf462ab78d6a
Richard Campen <richard@campen.co>
2022-12-31 17:07:04 +1300

    chore: refactor existing key actions into their own functions

--- ted.py
+++ ted.py
@@ -4,6 +4,64 @@ import curses
 import math
 
 
+###########
+# actions #
+###########
+
+def backspace(stdscr, buffer, buffer_key, buffer_key_count, lines):
+    buffer[buffer_key] = buffer[buffer_key][:-1]
+    # TODO: probably a more efficient thing to do that this.
+    stdscr.deleteln()
+    stdscr.insertln()
+    y, _ = stdscr.getyx()
+    stdscr.move(y, 0)
+    stdscr.addstr(buffer[buffer_key])
+
+    return buffer, buffer_key, buffer_key_count, lines
+
+
+def move_up(stdscr, buffer, buffer_key, buffer_key_count, lines):
+    index = lines.index(buffer_key)
+    if index > 0:
+        buffer_key = lines[index - 1]
+        x = len(buffer[buffer_key])
+        y, _ = stdscr.getyx()
+        stdscr.move(y - 1, x)
+
+    return buffer, buffer_key, buffer_key_count, lines
+
+
+def move_down(stdscr, buffer, buffer_key, buffer_key_count, lines):
+    try:
+        index = lines.index(buffer_key)
+        if index < buffer_key_count:
+            buffer_key = lines[index + 1]
+            x = len(buffer[buffer_key])
+            y, _ = stdscr.getyx()
+            stdscr.move(y + 1, x)
+    except IndexError:
+        pass  # noop
+
+    return buffer, buffer_key, buffer_key_count, lines
+
+
+def new_line(stdscr, buffer, buffer_key, buffer_key_count, lines):
+    # add new empty item to buffer/lines state
+    buffer_key = buffer_key_count
+    buffer[buffer_key] = ''
+    lines.append(buffer_key)
+    buffer_key_count = buffer_key_count + 1
+    # update cursor
+    y, _ = stdscr.getyx()
+    stdscr.move(y + 1, 0)
+
+    return buffer, buffer_key, buffer_key_count, lines
+
+
+##########
+# screen #
+##########
+
 def _setup_screen(stdscr):
 
     max_y, max_x = stdscr.getmaxyx()
@@ -16,6 +74,10 @@ def _setup_screen(stdscr):
     stdscr.addstr(0, 0, title_bar, curses.A_REVERSE)
     stdscr.refresh()
 
+
+########
+# main #
+########
 
 def main():
     res = curses.wrapper(curses_main)
@@ -41,43 +103,13 @@ def curses_main(stdscr):
         key_value = stdscr.getkey()
 
         if key_value == 'KEY_BACKSPACE':
-            buffer[buffer_key] = buffer[buffer_key][:-1]
-            # TODO: probably a more efficient thing to do that this.
-            stdscr.deleteln()
-            stdscr.insertln()
-            y, _ = stdscr.getyx()
-            stdscr.move(y, 0)
-            stdscr.addstr(buffer[buffer_key])
-
+            buffer, buffer_key, buffer_key_count, lines = backspace(stdscr, buffer, buffer_key, buffer_key_count, lines)
         elif key_value == 'KEY_UP':
-            index = lines.index(buffer_key)
-            if index > 0:
-                buffer_key = lines[index - 1]
-                x = len(buffer[buffer_key])
-                y, _ = stdscr.getyx()
-                stdscr.move(y - 1, x)
-
+            buffer, buffer_key, buffer_key_count, lines = move_up(stdscr, buffer, buffer_key, buffer_key_count, lines)
         elif key_value == 'KEY_DOWN':
-            try:
-                index = lines.index(buffer_key)
-                if index < buffer_key_count:
-                    buffer_key = lines[index + 1]
-                    x = len(buffer[buffer_key])
-                    y, _ = stdscr.getyx()
-                    stdscr.move(y + 1, x)
-            except IndexError:
-                pass  # noop
-
+            buffer, buffer_key, buffer_key_count, lines = move_down(stdscr, buffer, buffer_key, buffer_key_count, lines)
         elif key_value == '\n':
-
-            # add new empty item to buffer/lines state
-            buffer_key = buffer_key_count
-            buffer[buffer_key] = ''
-            lines.append(buffer_key)
-            buffer_key_count = buffer_key_count + 1
-            # update cursor
-            y, _ = stdscr.getyx()
-            stdscr.move(y + 1, 0)
+            buffer, buffer_key, buffer_key_count, lines = new_line(stdscr, buffer, buffer_key, buffer_key_count, lines)
             continue
 
         else:
